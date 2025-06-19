99d469d1d477998946a8971edfce98b8c3e0e963
Juan Veronez <juan.monteirov@gmail.com>
2025-06-19 18:31:44 -0300

    feat: create an error instance for user already exists (duplicated email)

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -18,7 +19,11 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.exec({ email, name, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send({ message: 'Internal error' }) // TODO: fix me
   }
 
   return reply.status(201).send()
