c50c5f0f114a61defde7648403bf0667acbe9516
Juan Veronez <juan.monteirov@gmail.com>
2025-06-24 09:09:56 -0300

    fix: update in-memory users repo to each instance has its own state

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,20 +2,18 @@ import { Prisma, User } from 'generated/prisma'
 import { UsersRepository } from '../users-repository'
 import { randomUUID } from 'crypto'
 
-const users: User[] = []
-
 export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
   async create(data: Prisma.UserCreateInput) {
     const newUser = { ...data, id: randomUUID() }
-    users.push(newUser)
+    this.users.push(newUser)
 
     return newUser
   }
 
   async findByEmail(email: string) {
-    console.log(users)
-
-    const user = users.find((u) => u.email === email)
+    const user = this.users.find((u) => u.email === email)
     return user ?? null
   }
 }
