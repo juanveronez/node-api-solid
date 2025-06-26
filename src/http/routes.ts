1a697817b25d024f2717dd2316c9b32f18c3442a
Juan Veronez <juan.monteirov@gmail.com>
2025-06-26 08:12:52 -0300

    feat: create authentication route and controller on `/session`

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controller/register'
+import { authenticate } from './controller/authenticate'
 
 export async function appRouter(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
