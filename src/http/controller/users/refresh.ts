f6d10d5d09330ff5c19916860cfab0cabc2d769d
Juan Veronez <juan.monteirov@gmail.com>
2025-07-06 00:42:14 -0300

    feat(RBAC): add role to user authentication and refresh tokens

--- src/http/controller/users/refresh.ts
+++ src/http/controller/users/refresh.ts
@@ -3,10 +3,12 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export async function refresh(req: FastifyRequest, reply: FastifyReply) {
   req.jwtVerify({ onlyCookie: true })
 
-  const token = await reply.jwtSign({}, { sign: { sub: req.user.sub } })
+  const { role } = req.user
+
+  const token = await reply.jwtSign({ role }, { sign: { sub: req.user.sub } })
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     { sign: { sub: req.user.sub, expiresIn: '7d' } },
   )
 
